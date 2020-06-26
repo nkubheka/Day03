50d29bf1456902af393a3a31ceea428e1900ba31
Ntokozo Kubheka <nkubheka@c4r3s5.wethinkcode.co.za>
2020-06-26 13:23:20 +0200

    ex00

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -6,15 +6,18 @@
 /*   By: nkubheka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 08:45:44 by nkubheka          #+#    #+#             */
-/*   Updated: 2020/06/24 10:20:10 by nkubheka         ###   ########.fr       */
+/*   Updated: 2020/06/26 13:22:59 by nkubheka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-int main()
+void ft_ft(int *nbr)
 {
-	int num = 42;
-	int *ptr;
-		printf("%d", num);
+	*nbr = 42;
+}
+int main() {
+	int c = 0;
+	ft_ft(&c);
+	printf("%d\n", c);
 	return 0;
 }
